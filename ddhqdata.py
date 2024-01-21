676c124fc6b2847a2bec491fbd09ab09eaa77ea8
MilesKleiber <milesjkleiber@gmail.com>
2024-01-21 12:48:40 -0600

    - Implemented national delegate count data - Modified decimal formatting - Removed "%" from all outputs

--- ddhqdata.py
+++ ddhqdata.py
@@ -2,7 +2,7 @@ import requests
 import json
 import time
 import os
-import ddhqauth
+# import ddhqauth
 
 # This is a python script to use the bearer token provided from ddhqauth.py to pull election data from DecisionDeskHQ.
 #
@@ -53,6 +53,13 @@ if not os.path.exists(state_deleg_dir):
 else:
     print(f"Directory '{state_deleg_dir}' already exists. Continuing.")
 
+national_deleg_dir = filteredfp + 'national_delegates/'
+if not os.path.exists(national_deleg_dir):
+    os.makedirs(national_deleg_dir)
+    print(f"Directory '{national_deleg_dir}' created successfully.")
+else:
+    print(f"Directory '{national_deleg_dir}' already exists. Continuing.")
+
 
 def pull_data(url, categ):
     with open('bearer_token.txt', 'r') as token_val:
@@ -99,30 +106,32 @@ def state_filter():
             cand_percent = float(cand_votes / total_votes) * 100
         pfile_name = f"{cand_name}_{setstate}_percent.txt"
         with open(state_dir + pfile_name, 'w') as file:
-            file.write(f"{cand_percent:.2f}%")
+            file.write(f"{cand_percent:.1f}")
 
-    precincts_totalf = 'precincts_total_count.txt'
-    precincts_total = s_response_data['data'][0]['topline_results']['precincts']['total']
-    with open(state_dir + precincts_totalf, 'w') as ptfile:
-        ptfile.write(str(precincts_total))
-
-    precincts_reportingf = 'precincts_reporting_count.txt'
-    precincts_reporting = s_response_data['data'][0]['topline_results']['precincts']['reporting']
-    with open(state_dir + precincts_reportingf, 'w') as prfile:
-        prfile.write(str(precincts_reporting))
+    total_state_votes_in = s_response_data['data'][0]['topline_results']['total_votes']
+    estimated_state_votes_high = s_response_data['data'][0]['topline_results']['estimated_votes']['estimated_votes_high']
+    # precincts_totalf = 'precincts_total_count.txt'
+    # precincts_total = s_response_data['data'][0]['topline_results']['precincts']['total']
+    # with open(state_dir + precincts_totalf, 'w') as ptfile:
+    #     ptfile.write(str(precincts_total))
+    #
+    # precincts_reportingf = 'precincts_reporting_count.txt'
+    # precincts_reporting = s_response_data['data'][0]['topline_results']['precincts']['reporting']
+    # with open(state_dir + precincts_reportingf, 'w') as prfile:
+    #     prfile.write(str(precincts_reporting))
 
     precincts_reportingpf = 'precincts_reporting_percent.txt'
-    if precincts_reporting <= 0.0:
+    if total_state_votes_in <= 0.0:
         precincts_reporting_percent = 0.0
-    elif precincts_total <= 0.0:
+    elif estimated_state_votes_high <= 0.0:
         precincts_reporting_percent = 0.0
     else:
-        precincts_reporting_percent = float(precincts_reporting / precincts_total) * 100
+        precincts_reporting_percent = float(total_state_votes_in / estimated_state_votes_high) * 100
     with open(state_dir + precincts_reportingpf, 'w') as prpfile:
         if precincts_reporting_percent == 100.0:
-            prpfile.write("100%")
+            prpfile.write("100")
         else:
-            prpfile.write(f"{precincts_reporting_percent:.1f}%")
+            prpfile.write(f"{precincts_reporting_percent:.1f}")
 
         # UNCOMMENT FOR TEST PRINTOUT.
         # test_print(cand_name, cand_id, cand_votes, total_votes, cand_percent)
@@ -132,33 +141,42 @@ def delegate_filter():
     with open(state_deleg_dir + 'deleg_response_data.json', 'r') as d_response_file:
         d_response_data = json.load(d_response_file)
 
-    deleg_republican_data = None
+    state_delegate_republican_data = None
+    national_delegate_republican_data = None
     for party_data in d_response_data['delegates']:
         if party_data['name'] == 'Republican':
+            national_delegate_republican_data = party_data['national']
             for state_data in party_data['states']:
                 if state_data['state_name'] == setstate:
-                    deleg_republican_data = state_data['candidates']
-                    delegate_vote_total = state_data['total']
+                    state_delegate_republican_data = state_data['candidates']
+                    state_delegate_vote_total = state_data['total']
                     break
-    deleg_republican_ids = list(deleg_republican_data.keys())
+    state_delegate_republican_ids = list(state_delegate_republican_data.keys())
+    national_delegate_republican_ids = list(national_delegate_republican_data.keys())
 
     for candidate in d_response_data['candidates']:
         candidate_id = str(candidate['cand_id'])
-        if candidate_id in deleg_republican_ids:
+        if candidate_id in national_delegate_republican_ids:
+            cand_name = candidate['last_name']
+            file_name = f"{cand_name}_national_delegate_votes.txt"
+            with open(national_deleg_dir + file_name, 'w') as file:
+                file.write(str(national_delegate_republican_data[candidate_id]))
+        if candidate_id in state_delegate_republican_ids:
             cand_name = candidate['last_name']
             file_name = f"{cand_name}_{setstate}_delegate_votes.txt"
             with open(state_deleg_dir + file_name, 'w') as file:
-                file.write(str(deleg_republican_data[candidate_id]))
-            if delegate_vote_total <= 0.0:
+                file.write(str(state_delegate_republican_data[candidate_id]))
+            if state_delegate_vote_total <= 0.0:
                 cand_percent = 0.0
             else:
-                cand_percent = float(deleg_republican_data[candidate_id] / delegate_vote_total) * 100
+                cand_percent = float(state_delegate_republican_data[candidate_id] / state_delegate_vote_total) * 100
             pfile_name = f"{cand_name}_{setstate}_delegate_percent.txt"
             with open(state_deleg_dir + pfile_name, 'w') as file:
                 if cand_percent == 100.0:
-                    file.write("100%")
+                    file.write("100")
                 else:
-                    file.write(f"{cand_percent:.1f}%")
+                    file.write(f"{cand_percent:.1f}")
+
 
 def test_print(cand_name, cand_id, cand_votes, total_votes, cand_percent):
     print("Name: " + cand_name)
